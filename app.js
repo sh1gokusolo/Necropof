d69ebdfdd244cb996ce16b6f0ccb70d2fb6f872a
sh1gokusolo <141768555+sh1gokusolo@users.noreply.github.com>
2026-02-12 12:37:45 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,226 @@
+const _0x5f3071 = _0x2bf0;
+function _0x2bf0(_0x41895c, _0x5a05eb) {
+    const _0x27e9b9 = _0x27e9();
+    return _0x2bf0 = function(_0x2bf0d8, _0x590962) {
+        _0x2bf0d8 = _0x2bf0d8 - 0x1ea;
+        let _0x234b1c = _0x27e9b9[_0x2bf0d8];
+        return _0x234b1c;
+    }
+    ,
+    _0x2bf0(_0x41895c, _0x5a05eb);
+}
+function _0x27e9() {
+    const _0x58ef50 = ['Проверяй\x20юзернеймы\x20перед\x20сделкой', 'Пример,\x20как\x20оставлять\x20реквизиты:', 'Личные\x20сообщения', 'en-US', '354MywUkW', 'addEventListener', 'toLocaleString', 'copy', 'ru-RU', '1914304fIgQeg', 'Owner', 'The\x20buyer\x20should\x20add\x20a\x20payment\x20note\x20like\x20“paid\x20debt”\x20to\x20avoid\x20bank\x20hold\x20/\x20block.', 'успешных\x20сделок', 'Double-check\x20usernames\x20before\x20paying', 'top', 'innerHTML', 'Сделка\x20на\x20звёзды', 'Gift\x20to\x20guarantor', 'Direct\x20messages', 'closest', 'После\x20этого\x20ПРОДАВЕЦ\x20передаёт\x20мне\x20NFT-подарок.\x20Я\x20отправляю\x20скрин,\x20что\x20получил\x20его.\x20Продавец\x20тоже\x20фиксирует\x20у\x20себя\x20(скрин/видео).', 'PrintScreen', 'Банк', 'metaKey', 'contextmenu', 'Deal\x20for\x20Telegram\x20Stars', '1914952affcjh', 'querySelectorAll', 'forEach', 'ФИО', 'preventDefault', '3267OfZnMp', '[data-i18n]', '.lang-btn', 'stopPropagation', 'click', 'Передача\x20подарка\x20гаранту', 'Secure\x20deals\x20•\x20Low\x20commission\x20•\x20Guaranteed\x20transactions', 'Contacts\x20&\x20communities', 'key', 'clipboardData', 'remove', 'add', 'Новостной\x20канал', 'Bank', 'getElementById', 'textContent', '30\x20000+\x20участников', 'Example:\x20what\x20to\x20send\x20as\x20payment\x20details:', 'mainCard', '223652BfaXpl', 'mousemove', 'Example:\x20how\x20to\x20send\x20crypto\x20address:', 'wheel', 'Phone\x20/\x20card\x20number', 'Контакты\x20и\x20сообщества', '.lang-btn[data-lang=\x22', 'querySelector', 'DOMContentLoaded', 'fee\x20per\x20deal', 'transform', 'setData', 'active', 'Передача\x20реквизитов', 'Пример,\x20как\x20оставлять\x20адрес\x20крипты:', 'ctrlKey', 'data-i18n', 'Full\x20name', 'Гарант\x20сделок\x20•\x20Низкая\x20комиссия\x20•\x20Безопасные\x20сделки', 'Связь\x20для\x20оформление\x20сделок', 'successful\x20deals', 'now', 'Information', 'The\x20BUYER\x20transfers\x20money\x20to\x20the\x20SELLER\x20using\x20given\x20details,\x20and\x20only\x20after\x20that\x20the\x20buyer\x20receives\x20the\x20NFT\x20gift.', 'height', '19480ohZjFb', '237PEqlRM', 'width', '3415sWMrrS', 'keydown', 'Then\x20the\x20SELLER\x20sends\x20the\x20NFT\x20gift\x20to\x20me.\x20I\x20send\x20a\x20screenshot\x20confirming\x20I\x20got\x20it.\x20The\x20seller\x20also\x20records\x20it\x20(screenshot/video).', 'The\x20SELLER\x20gives\x20their\x20payment\x20details\x20to\x20the\x20BUYER.', '585298FnedIm', 'max', 'isIntersecting', 'deg)\x20rotateX(', 'ПОКУПАТЕЛЬ\x20переводит\x20деньги\x20ПРОДАВЦУ\x20по\x20реквизитам,\x20и\x20только\x20после\x20этого\x20сам\x20покупатель\x20получает\x20NFT-подарок\x20себе.', 'target', '𝗴𝗿𝗶𝘁𝗵', 'Если\x20сделка\x20на\x20телеграм-звёзды\x20—\x20продавцу\x20обязательно\x20указать\x20юзернейм\x20аккаунта,\x20на\x20который\x20присылать\x20звёзды.', 'text/plain', 'Payment\x20comment', 'TON\x20—', 'News\x20channel', 'touchend', 'classList', 'lang', '5060MhJZxj', 'clientX', 'observe', 'Чат\x20&\x20Маркет', 'Комментарий\x20к\x20переводу', 'deg)\x20translateZ(0)', 'getAttribute', 'Информация', 'How\x20a\x20deal\x20works', 'If\x20the\x20deal\x20is\x20for\x20Telegram\x20Stars,\x20the\x20seller\x20must\x20specify\x20the\x20username\x20of\x20the\x20account\x20to\x20receive\x20the\x20stars.'];
+    _0x27e9 = function() {
+        return _0x58ef50;
+    }
+    ;
+    return _0x27e9();
+}
+(function(_0x526527, _0x272c57) {
+    const _0x26bce0 = _0x2bf0
+      , _0xd805dc = _0x526527();
+    while (!![]) {
+        try {
+            const _0x6e165f = parseInt(_0x26bce0(0x202)) / 0x1 + -parseInt(_0x26bce0(0x222)) / 0x2 + parseInt(_0x26bce0(0x21c)) / 0x3 * (-parseInt(_0x26bce0(0x21b)) / 0x4) + -parseInt(_0x26bce0(0x21e)) / 0x5 * (parseInt(_0x26bce0(0x23f)) / 0x6) + parseInt(_0x26bce0(0x244)) / 0x7 + parseInt(_0x26bce0(0x1ea)) / 0x8 + parseInt(_0x26bce0(0x1ef)) / 0x9 * (parseInt(_0x26bce0(0x231)) / 0xa);
+            if (_0x6e165f === _0x272c57)
+                break;
+            else
+                _0xd805dc['push'](_0xd805dc['shift']());
+        } catch (_0x4b90b4) {
+            _0xd805dc['push'](_0xd805dc['shift']());
+        }
+    }
+}(_0x27e9, 0x316ff),
+document[_0x5f3071(0x240)](_0x5f3071(0x20a), () => {
+    const _0x468b5b = _0x5f3071
+      , _0x5a1baa = document['getElementById'](_0x468b5b(0x201))
+      , _0x1139b1 = document[_0x468b5b(0x1fd)]('dealsCount')
+      , _0xe49298 = document[_0x468b5b(0x1fd)]('langSwitch')
+      , _0xc67251 = document[_0x468b5b(0x1eb)](_0x468b5b(0x1f1))
+      , _0x4a1f93 = {
+        'ru': {
+            'title': _0x468b5b(0x228),
+            'subtitle': _0x468b5b(0x214),
+            'successDeals': _0x468b5b(0x247),
+            'commission': 'комиссия\x20за\x20сделку',
+            'info': _0x468b5b(0x238),
+            'contacts': _0x468b5b(0x207),
+            'dmTitle': _0x468b5b(0x23d),
+            'dmSub': _0x468b5b(0x215),
+            'owner': 'Владелец',
+            'birzhaMembers': _0x468b5b(0x1ff),
+            'mainChannel': _0x468b5b(0x1fb),
+            'reviews': '𝐈𝐦𝐩𝐚𝐥𝐚 𝐌𝐚𝐫𝐤𝐞𝐭',
+            'reviewsSub': _0x468b5b(0x234),
+            'how': 'Как\x20проходит\x20сделка',
+            'step1Title': _0x468b5b(0x20f),
+            'step1Text': 'ПРОДАВЕЦ\x20даёт\x20свои\x20реквизиты\x20ПОКУПАТЕЛЮ.',
+            'step2Title': _0x468b5b(0x1f4),
+            'step2Text': _0x468b5b(0x24f),
+            'step3Title': 'Оплата\x20и\x20выдача',
+            'step3Text': _0x468b5b(0x226),
+            'examplePayTitle': _0x468b5b(0x23c),
+            'payPhone': 'Номер\x20телефона/карты',
+            'payName': _0x468b5b(0x1ed),
+            'payBank': _0x468b5b(0x251),
+            'exampleCryptoTitle': _0x468b5b(0x210),
+            'tonLabel': 'TON\x20—',
+            'commentTitle': _0x468b5b(0x235),
+            'commentText': 'Покупателю\x20желательно\x20оставлять\x20комментарий\x20к\x20переводу:\x20«отдал\x20долг»\x20(или\x20похожий),\x20чтобы\x20банк\x20не\x20задержал/не\x20заблокировал\x20платёж.',
+            'starsTitle': _0x468b5b(0x24b),
+            'starsText': _0x468b5b(0x229),
+            'foot': _0x468b5b(0x23b)
+        },
+        'en': {
+            'title': _0x468b5b(0x228),
+            'subtitle': _0x468b5b(0x1f5),
+            'successDeals': _0x468b5b(0x216),
+            'commission': _0x468b5b(0x20b),
+            'info': _0x468b5b(0x218),
+            'contacts': _0x468b5b(0x1f6),
+            'dmTitle': _0x468b5b(0x24d),
+            'dmSub': 'Contact\x20to\x20arrange\x20a\x20deal',
+            'owner': _0x468b5b(0x245),
+            'birzhaMembers': '30,000+\x20members',
+            'mainChannel': _0x468b5b(0x22d),
+            'reviews': '𝐈𝐦𝐩𝐚𝐥𝐚 𝐌𝐚𝐫𝐤𝐞𝐭',
+            'reviewsSub': 'Reputation\x20&\x20Information',
+            'how': _0x468b5b(0x239),
+            'step1Title': 'Seller\x20gives\x20details',
+            'step1Text': _0x468b5b(0x221),
+            'step2Title': _0x468b5b(0x24c),
+            'step2Text': _0x468b5b(0x220),
+            'step3Title': 'Payment\x20and\x20release',
+            'step3Text': _0x468b5b(0x219),
+            'examplePayTitle': _0x468b5b(0x200),
+            'payPhone': _0x468b5b(0x206),
+            'payName': _0x468b5b(0x213),
+            'payBank': _0x468b5b(0x1fc),
+            'exampleCryptoTitle': _0x468b5b(0x204),
+            'tonLabel': _0x468b5b(0x22c),
+            'commentTitle': _0x468b5b(0x22b),
+            'commentText': _0x468b5b(0x246),
+            'starsTitle': _0x468b5b(0x254),
+            'starsText': _0x468b5b(0x23a),
+            'foot': _0x468b5b(0x248)
+        }
+    }
+      , _0x154219 = (_0xe542b, _0x3086f6, _0x356890) => _0xe542b + (_0x3086f6 - _0xe542b) * _0x356890;
+    let _0x5276e1 = 0x0
+      , _0x30b56a = 0x0
+      , _0x4fd937 = 0x0
+      , _0x29e3cc = 0x0;
+    document[_0x468b5b(0x240)](_0x468b5b(0x203), _0x565e9b => {
+        const _0x5d5264 = _0x468b5b
+          , _0x57adac = _0x5a1baa['getBoundingClientRect']();
+        _0x5276e1 = (_0x565e9b[_0x5d5264(0x232)] - (_0x57adac['left'] + _0x57adac[_0x5d5264(0x21d)] / 0x2)) / (_0x57adac[_0x5d5264(0x21d)] / 0x2),
+        _0x30b56a = (_0x565e9b['clientY'] - (_0x57adac[_0x5d5264(0x249)] + _0x57adac[_0x5d5264(0x21a)] / 0x2)) / (_0x57adac[_0x5d5264(0x21a)] / 0x2);
+    }
+    );
+    const _0x4bfc76 = () => {
+        const _0x394d12 = _0x468b5b;
+        _0x4fd937 = _0x154219(_0x4fd937, _0x5276e1, 0.08),
+        _0x29e3cc = _0x154219(_0x29e3cc, _0x30b56a, 0.08),
+        _0x5a1baa['style'][_0x394d12(0x20c)] = 'perspective(900px)\x20rotateY(' + _0x4fd937 * 0x4 + _0x394d12(0x225) + _0x29e3cc * -0x4 + _0x394d12(0x236),
+        requestAnimationFrame(_0x4bfc76);
+    }
+    ;
+    _0x4bfc76();
+    const _0x10dbfc = 0x7D0;
+    let _0x1ec154 = 0x0
+      , _0x2762ee = ![];
+    const _0x371ec0 = () => {
+        if (_0x2762ee)
+            return;
+        _0x2762ee = !![];
+        const _0x2c47a4 = () => {
+            const _0x1c0aa2 = _0x2bf0
+              , _0x1c7b4a = _0x10dbfc - _0x1ec154
+              , _0x1fa2f6 = Math[_0x1c0aa2(0x223)](0x1, Math['floor'](_0x1c7b4a * 0.15));
+            _0x1ec154 += _0x1fa2f6;
+            if (_0x1ec154 >= _0x10dbfc)
+                _0x1ec154 = _0x10dbfc;
+            _0x1139b1[_0x1c0aa2(0x1fe)] = _0x1ec154[_0x1c0aa2(0x241)]('ru-RU') + '+';
+            if (_0x1ec154 < _0x10dbfc) {
+                const _0x1cb85c = _0x1c7b4a > 0x320 ? 0x23 : _0x1c7b4a > 0x96 ? 0x32 : 0x50;
+                setTimeout(_0x2c47a4, _0x1cb85c);
+            }
+        }
+        ;
+        _0x2c47a4();
+    }
+      , _0x4dd342 = new IntersectionObserver(_0x22b34a => {
+        _0x22b34a['forEach'](_0xda6bb3 => {
+            const _0x5c476a = _0x2bf0;
+            if (_0xda6bb3[_0x5c476a(0x224)])
+                _0x371ec0();
+        }
+        );
+    }
+    ,{
+        'threshold': 0.45
+    });
+    _0x4dd342[_0x468b5b(0x233)](_0x1139b1);
+    const _0x4dc62b = _0x2dfafa => {
+        const _0x54a291 = _0x468b5b
+          , _0x26faf2 = _0x4a1f93[_0x2dfafa];
+        document['documentElement'][_0x54a291(0x230)] = _0x2dfafa,
+        document[_0x54a291(0x1eb)](_0x54a291(0x1f0))[_0x54a291(0x1ec)](_0x36270c => {
+            const _0x3fa922 = _0x54a291
+              , _0x51e93d = _0x36270c[_0x3fa922(0x237)](_0x3fa922(0x212));
+            if (_0x26faf2[_0x51e93d])
+                _0x36270c[_0x3fa922(0x24a)] = _0x26faf2[_0x51e93d];
+        }
+        ),
+        _0xc67251[_0x54a291(0x1ec)](_0x16a9cc => _0x16a9cc[_0x54a291(0x22f)][_0x54a291(0x1f9)](_0x54a291(0x20e)));
+        const _0x2c96a5 = document[_0x54a291(0x209)](_0x54a291(0x208) + _0x2dfafa + '\x22]');
+        if (_0x2c96a5)
+            _0x2c96a5['classList'][_0x54a291(0x1fa)](_0x54a291(0x20e));
+        _0x2dfafa === 'en' ? _0x1139b1[_0x54a291(0x1fe)] = _0x1ec154[_0x54a291(0x241)](_0x54a291(0x23e)) + '+' : _0x1139b1[_0x54a291(0x1fe)] = _0x1ec154[_0x54a291(0x241)](_0x54a291(0x243)) + '+';
+    }
+    ;
+    _0xe49298[_0x468b5b(0x240)](_0x468b5b(0x1f3), _0x3f065c => {
+        const _0x3dfc01 = _0x468b5b
+          , _0x9d10a2 = _0x3f065c[_0x3dfc01(0x227)][_0x3dfc01(0x24e)](_0x3dfc01(0x1f1));
+        if (!_0x9d10a2)
+            return;
+        const _0x4d0f69 = _0x9d10a2['dataset'][_0x3dfc01(0x230)];
+        _0x4dc62b(_0x4d0f69);
+    }
+    ),
+    document['addEventListener'](_0x468b5b(0x253), _0x47dfba => _0x47dfba[_0x468b5b(0x1ee)]()),
+    document['addEventListener'](_0x468b5b(0x242), _0xbf5c4d => {
+        const _0x4f041b = _0x468b5b;
+        _0xbf5c4d[_0x4f041b(0x1ee)](),
+        _0xbf5c4d[_0x4f041b(0x1f8)]?.[_0x4f041b(0x20d)](_0x4f041b(0x22a), '');
+    }
+    ),
+    window[_0x468b5b(0x240)](_0x468b5b(0x205), _0x43ff29 => {
+        const _0x2017b1 = _0x468b5b;
+        _0x43ff29[_0x2017b1(0x211)] && _0x43ff29[_0x2017b1(0x1ee)]();
+    }
+    , {
+        'passive': ![]
+    });
+    let _0x534e25 = 0x0;
+    document['addEventListener'](_0x468b5b(0x22e), _0x12e2de => {
+        const _0x360882 = _0x468b5b
+          , _0x8a3689 = Date[_0x360882(0x217)]();
+        _0x8a3689 - _0x534e25 <= 0x15e && _0x12e2de[_0x360882(0x1ee)](),
+        _0x534e25 = _0x8a3689;
+    }
+    , {
+        'passive': ![]
+    }),
+    document[_0x468b5b(0x240)](_0x468b5b(0x21f), _0xd7c4ae => {
+        const _0x69eaad = _0x468b5b
+          , _0x4d8c5d = _0xd7c4ae[_0x69eaad(0x1f7)]['toLowerCase']();
+        (_0xd7c4ae['key'] === _0x69eaad(0x250) || (_0xd7c4ae[_0x69eaad(0x211)] || _0xd7c4ae[_0x69eaad(0x252)]) && (_0x4d8c5d === 'c' || _0x4d8c5d === 'u' || _0x4d8c5d === 's' || _0x4d8c5d === 'p' || _0x4d8c5d === 'i') || _0xd7c4ae['ctrlKey'] && (_0x4d8c5d === '+' || _0x4d8c5d === '-' || _0x4d8c5d === '0') || _0xd7c4ae[_0x69eaad(0x252)] && (_0x4d8c5d === '+' || _0x4d8c5d === '-' || _0x4d8c5d === '0') || _0xd7c4ae['ctrlKey'] && _0xd7c4ae['shiftKey'] && _0x4d8c5d === 'i') && (_0xd7c4ae[_0x69eaad(0x1ee)](),
+        _0xd7c4ae[_0x69eaad(0x1f2)]());
+    }
+    ),
+    _0x4dc62b('ru');
+}
+));
+
